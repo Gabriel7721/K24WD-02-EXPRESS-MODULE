90afd7e18f2609f597524190919e21f4bd9aaa8a
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-02-26 21:26:26 +0700

    update app

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import { userRoutes } from "./modules/user/user.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+
+export function createApp() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cookieParser());
+
+  app.use("/api/user", userRoutes);
+
+  app.use(errorMiddleware);
+
+  return app;
+}
